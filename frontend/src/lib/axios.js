3bb3b7da062bfbd89e00e2197718ee78badcba1c
omsandeeppatil <omsandeeppatil02@gmail.com>
2024-09-15 21:25:46 +0530

    Uploading this

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1",
+	withCredentials: true,
+});
