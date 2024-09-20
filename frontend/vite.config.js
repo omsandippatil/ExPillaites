26afe1f002c1fd65a0c82062476fc0e45f6713e9
Om  Patil <omsandeeppatil02@gmail.com>
2024-09-20 18:07:04 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, 'frontend'), // Set the root directory to frontend
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, 'frontend', 'dist'), // Output to frontend/dist
-    emptyOutDir: true, // Clears the output directory before building
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'frontend', 'src'), // Adjust the alias for imports if needed
-    },
-  },
-});
+})
